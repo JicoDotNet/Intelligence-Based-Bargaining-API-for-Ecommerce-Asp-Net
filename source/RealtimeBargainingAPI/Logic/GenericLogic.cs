7a4825d377ba637147698331c3e486192d1b1819
Soubhik Nandy <nandy.soubhik@live.co.uk>
2024-03-11 14:30:05 +0530

    update

--- source/RealtimeBargainingAPI/Logic/GenericLogic.cs
+++ source/RealtimeBargainingAPI/Logic/GenericLogic.cs
@@ -10,7 +10,7 @@
 			}
 		}
 
-		public static string AzureStorageConnectionString { get { return "DefaultEndpointsProtocol=https;AccountName=watersstorage;AccountKey=WJVUYwPCUYxr4OuTiRzNz5KgmNC8sMdO3c1dzchDjgjI1Sk6GlHZk2DuJ+FMbU3VqJTwpBN6gA2a+AStM0oH4w==;EndpointSuffix=core.windows.net"; } }
+		public static string AzureStorageConnectionString { get { return "Your_Azure_Storage_Connection_String"; } }
 		public static string DefaultToken { get { return "bcd6b947-24c8-4151-956b-0e58bb504e84"; } }
 	}
 }
